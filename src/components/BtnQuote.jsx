e954c5603de6a1e51c35e8645fc8d9ab882134b6
Bruno Prado <103675938+Brunopradoguevara@users.noreply.github.com>
2023-08-27 20:49:10 -0500

    fix background

--- src/components/BtnQuote.jsx
+++ src/components/BtnQuote.jsx
@@ -5,24 +5,19 @@ const BtnQuote = ({setQuote,phrases,setNumberBg,numberBg}) => {
 
     
     const handleRandomPhrase = () =>{
-        console.log("El valor de numerRandom es: " + numberBg)
         let newNumberBackground = getElementRandomArray([1,2,3,4])
-        console.log("Nuevo bg: " + newNumberBackground)
         setQuote(getElementRandomArray(phrases)) 
 
         if(numberBg !== newNumberBackground){
           setNumberBg(newNumberBackground)
         }else if(numberBg === newNumberBackground && newNumberBackground === 4){
           newNumberBackground -= 2
-          console.log("Se resto 2")
           setNumberBg(newNumberBackground)
         }else{
           newNumberBackground +=1
-          console.log("se sumo 1")
           setNumberBg(newNumberBackground)
         }
       
-        console.log("fin del proceso")
     }
 
   return (
